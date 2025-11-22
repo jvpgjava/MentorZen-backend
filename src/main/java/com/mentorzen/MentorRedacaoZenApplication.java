18fe51dd98e4e2aeb8e1952357b13b6648fdbfd5
jvpgjava <jjdevv05@gmail.com>
2025-11-22 01:31:58 -0300

    feat: adição de inner class principal

---
+++ src/main/java/com/mentorzen/MentorRedacaoZenApplication.java
@@ -0,0 +1,12 @@
+package com.mentorzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorRedacaoZenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorRedacaoZenApplication.class, args);
+    }
+}
